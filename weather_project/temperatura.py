d0ac93ac27f53169e2dd055fba3a2ffae4d82eeb
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2021-09-27 11:53:51 -0300

    Alteração nos arquivos

--- weather_project/temperatura.py
+++ weather_project/temperatura.py
@@ -3,9 +3,9 @@ from pyowm import OWM
 chave_api = '9d23aeb1c193dbd4fd958ef184504a1e'
 
 def obtem_temperatura(local):
-    climate_api = OWM(chave_api)
+    climate_api = OWM(chave_api).weather_manager()
     local_observed = climate_api.weather_at_place(local)
-    local_climate = local_observed.get_weather()
-    temperatura = local_climate.get_temperature(unit='celsius')
+    local_climate = local_observed.weather
+    temperatura = local_climate.temperature(unit='celsius')
 
     return temperatura
