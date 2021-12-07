ab1363df589724effea893073d80c43d13fd3d68
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2021-12-07 16:38:31 -0300

    Modificado exemplo em "hr.py"
    Alterado string exibida no cálculo da folha de pagamento para cada funci
    onário. Classes representantes de funcionários foram movidas para o arqu
    ivo "employees.py".

--- Orientação a Objetos/exemplos/payroll-system/hr.py
+++ Orientação a Objetos/exemplos/payroll-system/hr.py
@@ -1,27 +1,13 @@
+from typing import overload
+from employees import *
+
+
 class FolhaDePagamento():
     def calcular_folha(self, funcionarios):
         print('Calculando Folha de Pagamento')
         print('=============================')
         for funcionario in funcionarios:
             print(
-                f"Folha de Pagamento para: {funcionario.id} - {funcionario.nome}")
-            print(f"- Valor a ser pago: {funcionario.calcular_folha()}")
+                f"Pagamento para: {funcionario.id} - {funcionario.nome}")
+            print(f"- Valor a ser pago: R$ {funcionario.calcular_folha():.2f}")
             print('')
-
-
-class Funcionario():
-    def __init__(self, id, nome):
-        self.id = id
-        self.nome = nome
-
-    def trabalhar():
-        pass
-
-
-class FuncionarioAssalariado(Funcionario):
-    def __init__(self, id, nome, salario_semanal):
-        super().__init__(id, nome)
-        self.salario_semanal = salario_semanal
-
-    def calcular_folha(self):
-        return self.salario_semanal
