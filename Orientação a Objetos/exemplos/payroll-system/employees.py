53ef4b168fe305319242b2b01785d810d22528e6
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2021-12-09 09:49:29 -0300

    Adicionado herança múltipla nas representações de funcionários
    As entidades nomeadas como "Funcionário[...]" se transformaram em uma herança múltipla de outras classes.

--- Orientação a Objetos/exemplos/payroll-system/employees.py
+++ Orientação a Objetos/exemplos/payroll-system/employees.py
@@ -1,64 +1,52 @@
+from hr import (
+    PoliticaAssalariado,
+    PoliticaPagoPorHora,
+    PoliticaComissionado
+)
+
+from productivity import (
+    FuncaoGerencia,
+    FuncaoSecretaria,
+    FuncaoVendas,
+    FuncaoFabrica
+)
+
 class Funcionario():
     def __init__(self, id, nome):
         self.id = id
         self.nome = nome
 
 
-class FuncionarioAssalariado(Funcionario):
-    def __init__(self, id, nome, salario_semanal):
-        super().__init__(id, nome)
-        self.salario_semanal = salario_semanal
-
-    def calcular_folha(self):
-        return self.salario_semanal
-
-
-class FuncionarioPagoPorHora(Funcionario):
-    def __init__(self, id, nome, horas_trabalhadas, ganho_por_hora):
-        super().__init__(id, nome)
-        self.horas_trabalhadas = horas_trabalhadas
-        self.ganho_por_hora = ganho_por_hora
-
-    def calcular_folha(self):
-        return self.horas_trabalhadas * self.ganho_por_hora
-
-
-class FuncionarioComissionado(FuncionarioAssalariado):
-    def __init__(self, id, nome, salario_semanal, comissao):
-        super().__init__(id, nome, salario_semanal)
-        self.comissao = comissao
-
-    def calcular_folha(self):
-        fixo = super().calcular_folha()
-        return fixo + self.comissao
-
-
 # SISTEMA DE PRODUTIVIDADE
 
 
-class Gerente(FuncionarioAssalariado):
-    def trabalhar(self, horas):
-        print(f"{self.nome} grita e xinga por {horas} horas.")
+class Gerente(Funcionario, FuncaoGerencia, PoliticaAssalariado):
+    def __init__(self, id, nome, salario_semanal):
+        PoliticaAssalariado.__init__(self, salario_semanal)
+        super().__init__(id, nome)
 
 
-class Secretaria(FuncionarioAssalariado):
-    def trabalhar(self, horas):
-        print(f"{self.nome} passa {horas} horas arrumando a papelada.")
-
-class Vendedor(FuncionarioComissionado):
-    def trabalhar(self, horas):
-        print(f"{self.nome} passa {horas} horas no telefone.")
+class Secretaria(Funcionario, FuncaoSecretaria, PoliticaAssalariado):
+    def __init__(self, id, nome, salario_semanal):
+        PoliticaAssalariado.__init__(self, salario_semanal)
+        super().__init__(id, nome)
 
 
-class TrabalhadorFabrica(FuncionarioPagoPorHora):
-    def trabalhar(self, horas):
-        print(f"{self.nome} fabrica bugigangas por {horas} horas.")
+class Vendedor(Funcionario, FuncaoVendas, PoliticaComissionado):
+    def __init__(self, id, nome, salario_semanal, comissao):
+        PoliticaComissionado.__init__(self, salario_semanal, comissao)
+        super().__init__(id, nome)
+
+
+
+class TrabalhadorFabrica(Funcionario, FuncaoFabrica, PoliticaPagoPorHora):
+    def __init__(self, id, nome, horas_trabalhadas, ganho_por_hora):
+        PoliticaPagoPorHora.__init__(self, horas_trabalhadas, ganho_por_hora)
+        super().__init__(id, nome)
 
 
 # HERANÇA MÚLTIPLA
-class SecretariaTemporaria(Secretaria, FuncionarioPagoPorHora):
+class SecretariaTemporaria(Funcionario, FuncaoSecretaria, PoliticaPagoPorHora):
     def __init__(self, id, nome, horas_trabalhadas, ganho_por_hora):
-        FuncionarioPagoPorHora.__init__(self,id,nome,horas_trabalhadas,ganho_por_hora)
-    
-    def calcular_folha(self):
-        return FuncionarioPagoPorHora.calcular_folha(self)
+        PoliticaPagoPorHora.__init__(self,horas_trabalhadas, ganho_por_hora)
+        super().__init__(id, nome)
