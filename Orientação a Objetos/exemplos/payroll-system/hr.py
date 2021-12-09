796202e2d7671966c134f0aacd52aed6eede5f23
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2021-12-09 09:53:09 -0300

    Mudança na classe FolhaDePagamento()
    Esta mudança foi realizada pois a política de pagamento é uma classe que centraliza e oferece interfaces de cálculo salarial para cada tipo de pagamento no sistema hipotético de folha de pagamento

--- Orientação a Objetos/exemplos/payroll-system/hr.py
+++ Orientação a Objetos/exemplos/payroll-system/hr.py
@@ -1,6 +1,4 @@
 from typing import overload
-from employees import *
-
 
 class FolhaDePagamento():
     def calcular_folha(self, funcionarios):
@@ -10,4 +8,33 @@ class FolhaDePagamento():
             print(
                 f"Pagamento para: {funcionario.id} - {funcionario.nome}")
             print(f"- Valor a ser pago: R$ {funcionario.calcular_folha():.2f}")
+            if funcionario.endereco:
+                print(' - Enviar para:') 
+                print(funcionario.endereco)
             print('')
+
+class PoliticaAssalariado:
+    def __init__(self, salario_semanal):
+        self.salario_semanal = salario_semanal
+
+    def calcular_folha(self):
+        return self.salario_semanal
+
+
+class PoliticaPagoPorHora:
+    def __init__(self, horas_trabalhadas, ganho_por_hora):
+        self.horas_trabalhadas = horas_trabalhadas
+        self.ganho_por_hora = ganho_por_hora
+
+    def calcular_folha(self):
+        return self.horas_trabalhadas * self.ganho_por_hora
+
+
+class PoliticaComissionado(PoliticaAssalariado):
+    def __init__(self, salario_semanal, comissao):
+        super().__init__(salario_semanal)
+        self.comissao = comissao
+
+    def calcular_folha(self):
+        fixo = super().calcular_folha()
+        return fixo + self.comissao
