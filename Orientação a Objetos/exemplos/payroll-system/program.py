5330797eff01a6b1cc2d7b0928e91a1a03163f8e
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2021-12-09 11:25:18 -0300

    Modificado program.py
    Modifiquei o fluxo principal para refletir as mudanças (adição de endereço).

--- Orientação a Objetos/exemplos/payroll-system/program.py
+++ Orientação a Objetos/exemplos/payroll-system/program.py
@@ -1,12 +1,28 @@
-from hr import *
-from employees import *
-from productivity import *
+import hr
+import employees as emp
+import productivity as prodc
+import contacts as con
 
-gerente = Gerente(1, 'Marty Garrison', 3000)
-secretaria = Secretaria(2, 'Linda Lee', 1500)
-vendedor = Vendedor(3, 'John Roe', 1000, 250)
-trab_fabrica = TrabalhadorFabrica(4, 'Dan Quincy', 40, 15)
-secretaria_temporaria = SecretariaTemporaria(id=5, nome='Jane Killy', horas_trabalhadas=40, ganho_por_hora=9)
+gerente = emp.Gerente(1, 'Marty Garrison', 3000)
+gerente.endereco = con.Address(
+    '121 Admin Road',
+    'Concord',
+    'NH',
+    '03301'
+)
+
+secretaria = emp.Secretaria(2, 'Linda Lee', 1500)
+secretaria.endereco = con.Address(
+    '67 Paperwork Ave.',
+    'Manchester',
+    'NH',
+    '03101'
+)
+
+vendedor = emp.Vendedor(3, 'John Roe', 1000, 250)
+trab_fabrica = emp.TrabalhadorFabrica(4, 'Dan Quincy', 40, 15)
+secretaria_temporaria = emp.SecretariaTemporaria(
+    id=5, nome='Jane Killy', horas_trabalhadas=40, ganho_por_hora=9)
 
 funcionarios = [
     gerente,
@@ -16,9 +32,9 @@ funcionarios = [
     secretaria_temporaria
 ]
 
-sis_produtividade = SistemaProdutividade()
+sis_produtividade = prodc.SistemaProdutividade()
 sis_produtividade.monitorar(funcionarios, 40)
 
-sis_folha_pg = FolhaDePagamento()
+sis_folha_pg = hr.FolhaDePagamento()
 
 sis_folha_pg.calcular_folha(funcionarios)
