d69a31caab7e8bc0369b7308e362c74e9fc493bf
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2021-12-09 11:24:28 -0300

    Modificado productivity.py
    Adicionei classes representantes de cada função exercida no contexto da empresa hipotética.

--- Orientação a Objetos/exemplos/payroll-system/productivity.py
+++ Orientação a Objetos/exemplos/payroll-system/productivity.py
@@ -5,3 +5,23 @@ class SistemaProdutividade():
         for funcionario in funcionarios:
             funcionario.trabalhar(horas)
         print('')
+
+
+class FuncaoGerencia:
+    def trabalhar(self, horas):
+        print(f"{self.nome} grita e xinga por {horas} horas.")
+
+
+class FuncaoSecretaria:
+    def trabalhar(self, horas):
+        print(f"{self.nome} passa {horas} horas arrumando a papelada.")
+
+
+class FuncaoVendas:
+    def trabalhar(self, horas):
+        print(f"{self.nome} passa {horas} horas no telefone.")
+
+
+class FuncaoFabrica:
+    def trabalhar(self, horas):
+        print(f"{self.nome} fabrica bugigangas por {horas} horas.")
